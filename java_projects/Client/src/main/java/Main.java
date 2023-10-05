493b83e85b34384d0ee581a626418cd364ea8440
jan1310 <jan.schiffer@hsw-stud.de>
2023-10-05 22:34:33 +0200

    add Player automatically

--- java_projects/Client/src/main/java/Main.java
+++ java_projects/Client/src/main/java/Main.java
@@ -16,8 +16,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException, PlayerAlreadyAddedException, PlayerNotInTheGameException, GameNotStartedException {
-        String ip = "";
-        String name = "";
+        String ip;
+        String name;
         if (args.length >= 2) {
             ip = args[0];
             name = args[1];
@@ -37,39 +37,24 @@ public class Main {
         Player player = new PlayerView(name, view);
         Socket socket = new Socket(ip, 10000);
         Game game = new GameClientProxy(socket);
+        game.addPlayer(player);
         boolean running = true;
         while (running) {
             System.out.println("PROTOCOL: 1. addPlayer; 2. removePlayer; 3. movePlayerLeft; 4. movePlayerRight; 5. movePlayerUp; 6. movePlayerDown; 7. startGame; 8. endConnection");
                 char choice = view.keyGetChar();
-                switch (choice) {
-                    case '1':
-                        game.addPlayer(player);
-                        break;
-                    case '2':
-                        game.removePlayer(player);
-                        break;
-                    case 'a':
-                        game.movePlayerLeft(player);
-                        break;
-                    case 'd':
-                        game.movePlayerRight(player);
-                        break;
-                    case 'w':
-                        game.movePlayerUp(player);
-                        break;
-                    case 's':
-                        game.movePlayerDown(player);
-                        break;
-                    case '7':
-                        game.startGame();
-                        break;
-                    case '8':
-                        game.endConnection();
-                        running = false;
-                        break;
-                    default:
-                        System.out.println("Wrong input");
+            switch (choice) {
+                case '2' -> game.removePlayer(player);
+                case 'a' -> game.movePlayerLeft(player);
+                case 'd' -> game.movePlayerRight(player);
+                case 'w' -> game.movePlayerUp(player);
+                case 's' -> game.movePlayerDown(player);
+                case '7' -> game.startGame();
+                case '8' -> {
+                    game.endConnection();
+                    running = false;
                 }
+                default -> System.out.println("Wrong input");
+            }
         }
     }
 }
